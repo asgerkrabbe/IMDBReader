e8a531356a3b7d8d049671944077023f6e4d1ee2
asgerkrabbe <asgerkrabbe@gmail.com>
2020-11-16 16:47:16 +0100

    update

--- src/com/kea/Main.java
+++ src/com/kea/Main.java
@@ -1,8 +1,20 @@
 package com.kea;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
+    public static void main(String[] args) throws IOException {
+
+        URL url = new URL("https://www.imdb.com/search/title/?title=star+wars");
+        Scanner sc = new Scanner(url.openStream());
+
+
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+                }
+            }
+        }
